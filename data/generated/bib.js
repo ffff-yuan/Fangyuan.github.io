dc587016e232518732221401455fc5a9d7a1bd78
ffff-yuan <ffffyuan@gmail.com>
2025-05-16 11:09:14 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,118 @@
+﻿const generatedBibEntries = {
+    "10192728": {
+        "author": "Singh, Ravi Inder and Lilhore, Umesh Kumar",
+        "booktitle": "2023 8th International Conference on Communication and Electronics Systems (ICCES)",
+        "doi": "10.1109/ICCES57224.2023.10192728",
+        "keywords": "Deep learning;Surveys;Geology;Industry applications;Water quality;Predictive models;Feature extraction;Water quality;Machine learning;Deep learning",
+        "number": "",
+        "pages": "1069-1074",
+        "title": "A Survey of Machine Learning Models for Water Quality Prediction",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2023"
+    },
+    "ZHU2022107": {
+        "abstract": "With the rapid increase in the volume of data on the aquatic environment, machine learning has become an important tool for data analysis, classification, and prediction. Unlike traditional models used in water-related research, data-driven models based on machine learning can efficiently solve more complex nonlinear problems. In water environment research, models and conclusions derived from machine learning have been applied to the construction, monitoring, simulation, evaluation, and optimization of various water treatment and management systems. Additionally, machine learning can provide solutions for water pollution control, water quality improvement, and watershed ecosystem security management. In this review, we describe the cases in which machine learning algorithms have been applied to evaluate the water quality in different water environments, such as surface water, groundwater, drinking water, sewage, and seawater. Furthermore, we propose possible future applications of machine learning approaches to water environments.",
+        "author": "Mengyuan Zhu and Jiawei Wang and Xiao Yang and Yu Zhang and Linyu Zhang and Hongqiang Ren and Bing Wu and Lin Ye",
+        "doi": "https://doi.org/10.1016/j.eehl.2022.06.001",
+        "issn": "2772-9850",
+        "journal": "Eco-Environment & Health",
+        "keywords": "Machine learning, Water quality, Evaluation, Prediction",
+        "number": "2",
+        "pages": "107-116",
+        "title": "A review of the application of machine learning in water quality evaluation",
+        "type": "article",
+        "url": "https://www.sciencedirect.com/science/article/pii/S2772985022000163",
+        "volume": "1",
+        "year": "2022"
+    },
+    "barzegar2020short": {
+        "author": "Barzegar, Rahim and Aalami, Mohammad Taghi and Adamowski, Jan",
+        "journal": "Stochastic Environmental Research and Risk Assessment",
+        "number": "2",
+        "pages": "415--433",
+        "publisher": "Springer",
+        "title": "Short-term water quality variable prediction using a hybrid CNN--LSTM deep learning model",
+        "type": "article",
+        "volume": "34",
+        "year": "2020"
+    },
+    "elmotawakkil2025advanced": {
+        "author": "Elmotawakkil, Abdessamad and Enneya, Nourddine and Bhagat, Suraj Kumar and Ouda, Mohamed Mohamed and Kumar, Vikram",
+        "journal": "Journal of Hydroinformatics",
+        "pages": "jh2025290",
+        "publisher": "IWA Publishing",
+        "title": "Advanced machine learning models for robust prediction of water quality index and classification",
+        "type": "article",
+        "year": "2025"
+    },
+    "guo2024intelligent": {
+        "author": "Guo, Hui and Chen, Zhiyuan and Teo, Fang Yenn",
+        "journal": "Water Practice \\& Technology",
+        "number": "11",
+        "pages": "4538--4555",
+        "publisher": "IWA Publishing",
+        "title": "Intelligent water quality prediction system with a hybrid CNN--LSTM model",
+        "type": "article",
+        "volume": "19",
+        "year": "2024"
+    },
+    "liu2023prediction": {
+        "author": "Liu, Yan Zheng and Chen, Zhiyuan",
+        "journal": "Water Quality Research Journal",
+        "number": "2",
+        "pages": "87--98",
+        "publisher": "IWA Publishing",
+        "title": "Prediction of biochemical oxygen demand with genetic algorithm-based support vector regression",
+        "type": "article",
+        "volume": "58",
+        "year": "2023"
+    },
+    "ooi2022bod5": {
+        "author": "Ooi, Kai Sheng and Chen, ZhiYuan and Poh, Phaik Eong and Cui, Jian",
+        "journal": "Water Supply",
+        "number": "1",
+        "pages": "1168--1183",
+        "publisher": "IWA Publishing",
+        "title": "BOD5 prediction using machine learning methods",
+        "type": "article",
+        "volume": "22",
+        "year": "2022"
+    },
+    "w12123399": {
+        "abstract": "A Convolutional Neural Network (CNN)-Long Short-Term Memory (LSTM) combined with a deep learning approach was created by combining CNN and LSTM networks simulated water quality including total nitrogen, total phosphorous, and total organic carbon. Water level and water quality data in the Nakdong river basin were collected from the Water Resources Management Information System (WAMIS) and the Real-Time Water Quality Information, respectively. The rainfall radar image and operation information of estuary barrage were also collected from the Korea Meteorological Administration. In this study, CNN was used to simulate the water level and LSTM used for water quality. The entire simulation period was 1 January 2016\u201316 November 2017 and divided into two parts: (1) calibration (1 January 2016\u20131 March 2017); and (2) validation (2 March 2017\u201316 November 2017). This study revealed that the performances of both of the CNN and LSTM models were in the \u201cvery good\u201d range with above the Nash\u2013Sutcliffe efficiency value of 0.75 and that those models well represented the temporal variations of the pollutants in Nakdong river basin (NRB). It is concluded that the proposed approach in this study can be useful to accurately simulate the water level and water quality.",
+        "article-number": "3399",
+        "author": "Baek, Sang-Soo and Pyo, Jongcheol and Chun, Jong Ahn",
+        "doi": "10.3390/w12123399",
+        "issn": "2073-4441",
+        "journal": "Water",
+        "number": "12",
+        "title": "Prediction of Water Level and Water Quality Using a CNN-LSTM Combined Deep Learning Approach",
+        "type": "Article",
+        "url": "https://www.mdpi.com/2073-4441/12/12/3399",
+        "volume": "12",
+        "year": "2020"
+    },
+    "zamani2023forecasting": {
+        "author": "Zamani, Mohammad G and Nikoo, Mohammad Reza and Jahanshahi, Sina and Barzegar, Rahim and Meydani, Amirreza",
+        "journal": "Environmental Science and Pollution Research",
+        "number": "59",
+        "pages": "124316--124340",
+        "publisher": "Springer",
+        "title": "Forecasting water quality variable using deep learning and weighted averaging ensemble models",
+        "type": "article",
+        "volume": "30",
+        "year": "2023"
+    },
+    "zhu2024application": {
+        "author": "Zhu, Yue",
+        "journal": "Discover Water",
+        "number": "1",
+        "pages": "1--17",
+        "publisher": "Springer",
+        "title": "Application of a QPSO-optimized CNN-LSTM model in water quality prediction",
+        "type": "article",
+        "volume": "4",
+        "year": "2024"
+    }
+};
